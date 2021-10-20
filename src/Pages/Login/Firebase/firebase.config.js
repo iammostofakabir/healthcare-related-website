6ae7c37d69446918ad5bfa04a29b135af2f32bd1
Mostofa Kabir <76174777+iammostofakabir@users.noreply.github.com>
2021-10-20 19:37:01 +0600

    Add files via upload

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzHuMkpoxM8Y6-YRxPmrbhVFsRNmKKi4c",
+  authDomain: "meditation-yoga-e9ac9.firebaseapp.com",
+  projectId: "meditation-yoga-e9ac9",
+  storageBucket: "meditation-yoga-e9ac9.appspot.com",
+  messagingSenderId: "120420911840",
+  appId: "1:120420911840:web:19a59f5c802ae271ea5e85"
+};
+
+export default firebaseConfig;
